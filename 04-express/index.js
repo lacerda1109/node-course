5b5ec11476695a346cea271b5df000ead0183af4
Gabriel Lacerda <devlacerda1109@gmail.com>
2023-08-21 16:49:39 -0300

    Adds route with params

--- 04-express/index.js
+++ 04-express/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Project root!");
+  res.send("Nothing here.");
 });
 
 app.get("/about", function (req, res) {
-  res.send("My about page");
+  res.send("My about page.");
+});
+
+app.get("/show-param/:param", function (req, res) {
+  res.send(`O valor enviado por parâmetro foi: ${req.params.param}`);
 });
 
 app.listen(8081, function () {
